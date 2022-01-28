a61daa879c81f730ba8a97a13db7b6b53247990c
hectorzaragoza <zaragoza09@gmail.com>
2022-01-28 17:27:28 -0600

    set up basic express socketio connection between chat.js and chat.html

--- socketio-learning/socket101/basics.js
+++ socketio-learning/socket101/basics.js
@@ -9,7 +9,20 @@ const server = http.createServer((req, res) => {
 })
 
 // Initializing a socketio server that takes the node http server
-const io = socketio(server);
+const io = socketio(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+//socket.io
+io.on('connect', (socket, req) => {
+    // we send method becomes socket emit method
+    socket.emit('Welcome to the websocket server!')
+    socket.on('message', (msg) => {
+        console.log(msg)
+    })
+})
 
 // Listen to port 8000
 server.listen(8000)
