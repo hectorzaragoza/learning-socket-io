d255dfabd28b92c8f1b28469463712bd206e64df
hectorzaragoza <zaragoza09@gmail.com>
2022-01-28 19:43:25 -0600

    Reviewed socket.io docs for client and server sides and created a simple chat application

--- socketio-learning/socket101/chat.js
+++ socketio-learning/socket101/chat.js
@@ -9,9 +9,14 @@ app.use(express.static(__dirname + '/public'))
 const expressServer = app.listen(9000)
 
 const io = socketio(expressServer)
+
 io.on('connection', (socket) => {
     socket.emit('messageFromServer', {data: 'Welcome to the socketio server'})
     socket.on('messageToServer', (dataFromClient) => {
         console.log(dataFromClient)
+    })
+    socket.on('newMessageToServer', (msg) => {
+        // console.log('Am I getting the message: ', msg)
+        io.emit('messageToClients', {text: msg.text})
     })
 })
