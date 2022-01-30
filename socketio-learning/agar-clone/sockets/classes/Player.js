eb7beb8078e616d7a6ed90af444cdfe90b22c7d9
hectorzaragoza <zaragoza09@gmail.com>
2022-01-30 13:00:43 -0600

    set up classes to compartmentalize information that everyone needs to know (clients to clients and clients to server) and what only server needs to know about each individual client to minimize bandwidth load

---
+++ socketio-learning/agar-clone/sockets/classes/Player.js
@@ -0,0 +1,8 @@
+// Data about a given player
+class Player {
+    constructor(socketId, playerConfig, playerData) {
+        socketId
+        playerConfig
+        playerData
+    }
+}
--- socketio-learning/agar-clone/sockets/socketMain.js
+++ socketio-learning/agar-clone/sockets/socketMain.js
@@ -1,11 +1,37 @@
 // Where all of our main socket stuff will go
 const io = require('../server').io
+
+// ========= Classes ===========
+const Player = require('./classes/Player')
+const PlayerData = require('./classes/PlayerData')
+const PlayerConfig = require('./classes/PlayerConfig')
 const Orb = require('./classes/Orb')
 let orbs = []
+let settings = {
+    defaultOrbs: 500,
+    defaultSpeed: 6,
+    defaultSize: 6,
+    defaultZoom: 1.5,
+    worldWidth: 500,
+    worldHeight: 500 
+}
 
 initGame()
 
+// Our connection between client and server. Io is for our socket.io server
+// that takes our express server as input,
+// sockets is the alias for the main namespace ('/') which
+// is what everyone connects to at the start. 
+// on is to register a new handler for the event on 
+// the main namespace
 io.sockets.on('connect', (socket) => {
+    // A player has connected
+    // Make a playerConfig object (1:1 between server and each client)
+    let playerConfig = new PlayerConfig(settings)
+    // Make a playerData object (Everyone needs this)
+    let playerData = new PlayerData(null, settings)
+    // Make a master player object to hold both
+    let player = new Player(socket.id, playerConfig, playerData)
     socket.emit('init', {
         orbs
     })
@@ -13,8 +39,8 @@ io.sockets.on('connect', (socket) => {
 
 // Run at beginning of new game
 function initGame() {
-    for(let i = 0; i < 500; i++) {
-        orbs.push(new Orb())
+    for(let i = 0; i < settings.defaultOrbs; i++) {
+        orbs.push(new Orb(settings))
     }
 }
 
