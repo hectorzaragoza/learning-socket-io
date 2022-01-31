d535ba15a66737442030bc6b59c106547c988905
hectorzaragoza <zaragoza09@gmail.com>
2022-01-31 15:41:47 -0600

    initial commit for performance monitor

---
+++ socketio-learning/performance-monitor/nodeClient/index.js
@@ -0,0 +1,6 @@
+// Node will capture local performance data to send to
+// socket.io server
+
+// We'll use the modules:
+// -- Farmhash - Google module to do hashing algorithms
+// -- socket.io-client
---
+++ socketio-learning/performance-monitor/server/server.js
@@ -0,0 +1,7 @@
+// This will serve the socket.io server that will service
+// both node and react clients
+
+// We'll use:
+// -- socket.io
+// -- socket.io-redis
+// -- farmhash
